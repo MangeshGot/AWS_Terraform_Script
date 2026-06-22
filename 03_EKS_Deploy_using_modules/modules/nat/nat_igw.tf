25311565941fa46a32241841b0687d3b973e7f0c
MangeshGot <m.sonawanegot@gmail.com>
2026-06-22 23:13:34 +0530

    Changes

--- 03_EKS_Deploy_using_modules/modules/nat/nat_igw.tf
+++ 03_EKS_Deploy_using_modules/modules/nat/nat_igw.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "_nat_igw" {
   allocation_id=aws_eip.nat_eip.id
-  subnet_id=module.subnets.public_subnet_1_id #IMPORTANT: NAT Gateway must be created in a public subnet
+  subnet_id=aws_subnet.public_subnet_1.id #IMPORTANT: NAT Gateway must be created in a public subnet
   tags = {Name="${var.environment}-NAT-IGW"}
 }
--- 03_EKS_Deploy_using_modules/modules/subnets/subnets.tf
+++ 03_EKS_Deploy_using_modules/modules/subnets/subnets.tf
@@ -1,23 +1,23 @@
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = module.vpc.vpc_id
+  vpc_id            = aws_vpc.vpc_main.id
   cidr_block        = var.public_subnet_1
   availability_zone = var.az_1a
   tags              = { Name = "${var.environment}-PUBLIC-SUBNET-1" }
 }
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = module.vpc.vpc_id
+  vpc_id            = aws_vpc.vpc_main.id
   cidr_block        = var.public_subnet_2
   availability_zone = var.az_1b
   tags              = { Name = "${var.environment}-PUBLIC-SUBNET-2" }
 }
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = module.vpc.vpc_id
+  vpc_id            = aws_vpc.vpc_main.id
   cidr_block        = var.private_subnet_1
   availability_zone = var.az_1c
   tags              = { Name = "${var.environment}-PRIVATE-SUBNET-1" }
 }
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = module.vpc.vpc_id
+  vpc_id            = aws_vpc.vpc_main.id
   cidr_block        = var.private_subnet_2
   availability_zone = var.az_1d
   tags              = { Name = "${var.environment}-PRIVATE-SUBNET-2" }
