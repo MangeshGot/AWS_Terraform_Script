912308803fd0cbb10506fd82bec7a2c269352c32
MangeshGot <m.sonawanegot@gmail.com>
2026-06-24 00:21:43 +0530

    EKS VPC Created seperate module for each

---
+++ 03_EKS_Deploy_using_modules/enviroment/production/variables.tf
@@ -0,0 +1,39 @@
+variable "eks_vpc_region" {
+  type = string
+}
+variable "eks_access_key" {
+  type = string
+}
+variable "eks_secret_key" {
+  type = string
+}
+variable "eks_vpc_cidr_block" {
+  type = string
+}
+variable "eks_public_subnet_1" {
+  type = string
+}
+variable "eks_public_subnet_2" {
+  type = string
+}
+variable "eks_private_subnet_1" {
+  type = string
+}
+variable "eks_private_subnet_2" {
+  type = string
+}
+variable "environment" {
+  type=string
+}
+variable "az_1a" {
+  type=string
+}
+variable "az_1b" {
+  type=string
+}
+variable "az_1c" {
+  type=string
+}
+variable "az_1d" {
+  type=string
+}
