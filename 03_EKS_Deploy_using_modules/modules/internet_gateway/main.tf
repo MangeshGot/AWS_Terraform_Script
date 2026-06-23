d718c1bf47f059810a9c8aaa4d4ab1fa17b98542
MangeshGot <m.sonawanegot@gmail.com>
2026-06-24 00:20:34 +0530

    EKS VPC Created seperate module for each

--- 03_EKS_Deploy_using_modules/modules/internet_gateway/main.tf
+++ 03_EKS_Deploy_using_modules/modules/internet_gateway/main.tf
@@ -8,5 +8,5 @@ resource "aws_eip" "eks_nat_igw_eip" {
 resource "aws_nat_gateway" "eks_nat_igw" {
   allocation_id = aws_eip.eks_nat_igw_eip.id
   subnet_id     = var.eks_public_subnet_1_id #IMPORTANT!! To Be Created in Public Subnet Only
-  tags = { Name = "${var.environment}-EKS-IGW" }
+  tags = { Name = "${var.environment}-EKS-NAT-IGW" }
 }
