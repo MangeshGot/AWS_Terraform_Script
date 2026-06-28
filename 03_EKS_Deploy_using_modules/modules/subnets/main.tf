9e92223040f0c2591382fa5c05485de9ca3acc36
MangeshGot <m.sonawanegot@gmail.com>
2026-06-29 00:40:37 +0530

    subnet changes

--- 03_EKS_Deploy_using_modules/modules/subnets/main.tf
+++ 03_EKS_Deploy_using_modules/modules/subnets/main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "eks_public_subnet_2"{
 resource "aws_subnet" "eks_private_subnet_1"{
     vpc_id =var.eks_vpc_id
     cidr_block=var.eks_private_subnet_1
-    availability_zone= var.az_1c
+    availability_zone= var.az_1a
     tags ={
         Name="${var.environment}-Private-Subnet-1"
         "kubernetes.io/role/internal-elb" = "1"
@@ -32,7 +32,7 @@ resource "aws_subnet" "eks_private_subnet_1"{
 resource "aws_subnet" "eks_private_subnet_2"{
     vpc_id =var.eks_vpc_id
     cidr_block=var.eks_private_subnet_2
-    availability_zone= var.az_1d
+    availability_zone= var.az_1b
     tags ={
         Name="${var.environment}-Private-Subnet-2"
         "kubernetes.io/role/internal-elb" = "1"
